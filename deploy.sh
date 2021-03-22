3373f2d1858bbe75096cf33e8157b6f7eeb793fe
shizhikai <shizhikai@cmcm.com>
2021-03-22 21:17:08 +0800

    edit

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:shikkk/shikkk.github.io.git master
-
+git push -f git@github.com:shikkk/myBlog.git master
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
